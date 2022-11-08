79f91cce9c1282bc188b4ef003c69636d8000d44
Queite <queitesc@gmail.com>
2022-11-08 18:01:59 -0300

    feat: :sparkles: adiciona scrap que retorna dict

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,9 +32,25 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css(".comment").getall()),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        "tags": selector.css(".post-tags li a[rel=tag]::text").getall(),
+        "category": selector.css(".label::text").get(),
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# content = fetch("https://blog.betrybe.com/tecnologia/sgbd-tudo-sobre/")
+# print(scrape_noticia(content))
