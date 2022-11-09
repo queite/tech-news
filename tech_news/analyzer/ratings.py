6fb9063cc258a407309e4c9db05ad5f4b0c5d9d9
Queite <queitesc@gmail.com>
2022-11-09 14:23:03 -0300

    feat: :sparkles: implementa função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,18 @@
+from operator import itemgetter
+
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news.sort(key=itemgetter("comments_count", "title"), reverse=True)
+    return [(item["title"], item["url"]) for item in news[:5]]
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+
+
+top_5_categories()
