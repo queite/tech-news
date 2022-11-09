c55a2ae3a2744144d6317eeb2648a6f73d1a3c9b
Queite <queitesc@gmail.com>
2022-11-08 21:08:07 -0300

    feat: :sparkles: implementa função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from tech_news.database import search_news
 
 
@@ -10,7 +12,13 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        iso_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    bd_format_date = "{:%d/%m/%Y}".format(iso_date)
+    news = search_news({"timestamp": bd_format_date})
+    return [(item["title"], item["url"]) for item in news]
 
 
 # Requisito 8
@@ -21,3 +29,6 @@ def search_by_tag(tag):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# print(search_by_date("2022-10-28"))
